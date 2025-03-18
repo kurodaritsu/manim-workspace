8b7e8db1ee92ff725748db7bc3a9979fd5a9ec33
KaidenFrizu <39328974+KaidenFrizu@users.noreply.github.com>
2025-03-18 12:42:39 +0000

    Provide details on example files

--- src/example_scene.py
+++ src/example_scene.py
@@ -1,3 +1,6 @@
+# For more code examples, visit:
+# https://docs.manim.community/en/stable/examples.html
+
 from manim import *
 
 
@@ -12,4 +15,25 @@ class SquareToCircle(Scene):
         self.play(Create(square))
         self.play(Transform(square, circle))
         self.play(FadeOut(square))
+        self.wait()
+
+
+class MovingFrameBox(Scene):
+    def construct(self):
+        text=MathTex(
+            "\\frac{d}{dx}f(x)g(x)=",
+            "f(x)\\frac{d}{dx}g(x)",
+            "+",
+            "g(x)\\frac{d}{dx}f(x)"
+        )
+        self.play(Write(text))
+        framebox1 = SurroundingRectangle(text[1], buff = .1)
+        framebox2 = SurroundingRectangle(text[3], buff = .1)
+        self.play(
+            Create(framebox1),
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox1,framebox2),
+        )
         self.wait()
